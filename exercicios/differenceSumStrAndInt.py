649ef69144c1feb64c8a787bb37b1534cd0bfd08
brenocesar83 <33488451+brenocesar83@users.noreply.github.com>
2017-11-08 16:01:48 -0200

    Create differenceSumStrAndInt.py

---
+++ exercicios/differenceSumStrAndInt.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite o segundo numero: "))
+soma = n1+n2
+#print("A soma eh: ", n1+n2)
+print("the sum of {} and {} is {} " .format(n1,n2,soma))
+
