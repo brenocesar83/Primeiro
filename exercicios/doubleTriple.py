26bd7e615a448b7c3a93e05a3d75bd03ab93b2f9
brenocesar83 <33488451+brenocesar83@users.noreply.github.com>
2017-11-08 16:02:26 -0200

    Create doubleTriple.py

---
+++ exercicios/doubleTriple.py
@@ -0,0 +1,6 @@
+n = int(input("Input a number: "))
+print("The number before {} is {} end the after is {}".format(n, n-1, n+1))
+
+n2 = int(input("Input a number: "))
+print("The double for {} is {}, the triple is {}".format(n2, n*2, n*3), end=' ')
+print("and the square root is {}".format(n2**(1/2)))
