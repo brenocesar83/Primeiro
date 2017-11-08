dae414e06e1d4a01b5a94f5188fe67caae2022c0
brenocesar83 <33488451+brenocesar83@users.noreply.github.com>
2017-11-08 14:41:24 -0200

    Create allLotoFacilResultsInCSV.py
    This file connect to lottery Caixa site and downloads the zipFile that contains a html file with all results.
    Then, access the html file in that zip and convert it to CSV that contains only the concurse number, date and the balls.

---
+++ allLotoFacilResultsInCSV.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+import csv
+import zipfile
+import os
+
+# Caminho para download do arquivo de resultados
+comando = "wget -N 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip'"
+os.system(comando)
+
+# Extraindo o zip
+zFile = zipfile.ZipFile("D_lotfac.zip")
+arquivo = zFile.open(zFile.infolist()[0])
+
+
+#arquivo = open("/home/cabox/workspace/D_LOTFAC.HTM", encoding='latin-1')
+soup = BeautifulSoup(arquivo, "html.parser")  # html.parser html5lib lxml
+samples = soup.find("table")
+
+# Separa apenas linha 
+def titulo(content, tag):
+    count = 0
+    titles = []
+    for row in content.find_all(tag):
+        count = count + 1
+        titles.append(row.text.strip())
+        if count > 16:
+            break
+    return titles
+
+# Separa valores de cada coluna isolando em linhas 
+def resultados(content, tag, tag2):
+    linhas = []
+    for row in content.find_all(tag):
+        count = 0
+        colunas = []
+        for row2 in row.find_all(tag2):
+            count = count + 1
+            if row2.has_attr('rowspan'):
+                colunas.append(row2.text.strip())
+            else:
+                break
+            if count > 16:
+                break
+        if len(colunas) > 1:
+            linhas.append(colunas)
+    return linhas
+
+
+def escreverArquivo():
+    file = open("/home/cabox/workspace/lotofacil.csv", 'w')
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    rs = resultados(samples, "tr", "td")
+    writer.writerow(titulo(samples, "th"))
+    for row in rs:
+        writer.writerow(row)
+    file.close()
+
+escreverArquivo()
+
+
+def lerArquivo():
+    file = open("/home/cabox/workspace/lotofacil.csv", newline='')
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+lerArquivo()
