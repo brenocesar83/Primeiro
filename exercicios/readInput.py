8d35a38514c01f551ab45f6ae3a5566196f1dddf
brenocesar83 <33488451+brenocesar83@users.noreply.github.com>
2017-11-08 16:00:15 -0200

    Create readInput.py

---
+++ exercicios/readInput.py
@@ -0,0 +1,7 @@
+nome = input("Qual a seu nome? ")
+print("Bom dia, ",nome)
+
+dia = input("Qual o dia que voce nasceu? ")
+mes = input("Qual o mes que voce nasceu? ")
+ano = input("Qual o ano que voce nasceu? ")
+print("Ola ",nome,"! Voce nasceu no dia ",dia, "de ",mes, "de ",ano)
